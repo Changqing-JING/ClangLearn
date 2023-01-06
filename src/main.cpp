1d50afcdc3ba80320ea330a350cd1708bb23a5c8
Changqing Jing <changqing.jing@bmw.com>
2023-01-06 10:35:42 +0800

    add clang format

--- src/main.cpp
+++ src/main.cpp
@@ -7,29 +7,27 @@ std::ostream &operator<<(std::ostream &stream, const CXString &str) {
   return stream;
 }
 
+CXChildVisitResult printCursor(CXCursor cursor, CXCursor parent, CXClientData client_data) {
+  CXCursorKind cursorKind = clang_getCursorKind(cursor);
+  std::cout << "Cursor '" << clang_getCursorSpelling(cursor) << "' of kind '" << clang_getCursorKindSpelling(cursorKind) << std::endl;
+  clang_visitChildren(cursor, printCursor, nullptr);
+  return CXChildVisit_Recurse;
+}
+
 int main(int argc, const char *argv[]) {
   if (argc < 2) {
     std::cout << "no input file" << std::endl;
     exit(1);
   }
   CXIndex index = clang_createIndex(0, 0);
-  CXTranslationUnit unit = clang_parseTranslationUnit(
-      index, argv[1], nullptr, 0, nullptr, 0, CXTranslationUnit_None);
+  CXTranslationUnit unit = clang_parseTranslationUnit(index, argv[1], nullptr, 0, nullptr, 0, CXTranslationUnit_None);
   if (unit == nullptr) {
     std::cerr << "Unable to parse translation unit. Quitting." << std::endl;
     exit(-1);
   }
 
   CXCursor cursor = clang_getTranslationUnitCursor(unit);
-  clang_visitChildren(
-      cursor,
-      [](CXCursor c, CXCursor parent, CXClientData client_data) {
-        CXCursorKind cursorKind = clang_getCursorKind(c);
-        std::cout << "Cursor '" << clang_getCursorSpelling(c) << "' of kind '"
-                  << clang_getCursorKindSpelling(cursorKind) << std::endl;
-        return CXChildVisit_Recurse;
-      },
-      nullptr);
+  clang_visitChildren(cursor, printCursor, nullptr);
 
   clang_disposeTranslationUnit(unit);
   clang_disposeIndex(index);
